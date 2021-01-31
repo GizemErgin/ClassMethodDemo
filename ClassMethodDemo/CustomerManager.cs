3aebb46b36428b051291fbda95350aab0243b763
gizemergin93@gmail.com <gizemergin93@gmail.com>
2021-02-01 02:47:18 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+       public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Id: ");
+            customer.CustomerId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nCustomer TC Number: ");
+            customer.CustomerTcNumber = Console.ReadLine();
+            Console.WriteLine("\nCustomer Name: ");
+            customer.CustomerName = Console.ReadLine();
+            Console.WriteLine("\nCustomer Surname: ");
+            customer.CustomerSurname = Console.ReadLine();
+
+            Console.WriteLine("\nSuccessfully added to the customer list: "+ customer.CustomerName+" "+customer.CustomerSurname);
+        }
+
+        public void Delete(List<Customer> customers, int ID)
+        {
+
+            foreach (var c in customers)
+            {
+                if (c.CustomerId == ID)
+                {
+                    Console.WriteLine("\nSuccessfully deleted from the customer list: " + c.CustomerName + " " + c.CustomerSurname);
+                }
+            }
+           
+        }
+
+        public void Listing(List<Customer> customers)
+        {
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("ID: "+customer.CustomerId + " \nTC Number: "+customer.CustomerTcNumber
+                    + "\nName Surname: "+customer.CustomerName + " "+customer.CustomerSurname);
+                Console.WriteLine("----------------");
+            }
+
+
+        }
+
+        
+
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            {
+                customer1.CustomerId = 1;
+                customer1.CustomerTcNumber = "11111111111";
+                customer1.CustomerName = "Gizem";
+                customer1.CustomerSurname = "Ergin";
+            }
+
+            Customer customer2 = new Customer();
+            {
+                customer2.CustomerId = 2;
+                customer2.CustomerTcNumber = "22222222222";
+                customer2.CustomerName = "Ece";
+                customer2.CustomerSurname = "Güler";
+            }
+
+            Customer customer3 = new Customer();
+            {
+                customer3.CustomerId = 3;
+                customer3.CustomerTcNumber = "33333333333";
+                customer3.CustomerName = "Ezgi";
+                customer3.CustomerSurname = "Demir";
+            }
+
+            Customer customer4 = new Customer();
+            {
+                customer4.CustomerId = 4;
+                customer4.CustomerTcNumber = "44444444444";
+                customer4.CustomerName = "Hanife";
+                customer4.CustomerSurname = "Tuna";
+            }
+
+            Selection();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            void Selection()
+            {
+                Console.WriteLine("Select an Action \nAdded (a/A) -- Delete (d/D) -- Listing (l/L)");
+                var pressed = Console.ReadLine();
+                switch (pressed)
+                {
+                    case "a":
+                    case "A":
+                        {
+                            Console.Clear();
+                            Customer customer = new Customer();
+                            CustomerManager customerManager = new CustomerManager();
+                            customerManager.Add(customer);
+                            Console.WriteLine("");
+                            Selection();
+                            break;
+                        }
+                    case "d":
+                    case "D":
+                        {
+                            Console.Clear();
+                            CustomerManager customerManager = new CustomerManager();
+                            List<Customer> customers = new List<Customer> { customer1, customer2, customer3, customer4 };
+                            foreach (var c in customers)
+                            {
+                                Console.WriteLine("Customer ID: " + c.CustomerId + " Customer Name Surname: " + c.CustomerName + " " + c.CustomerSurname);
+                            }
+                            Console.WriteLine("\nCustomer Id for delete customer: ");
+                            int ID = Convert.ToInt32(Console.ReadLine());
+                            customerManager.Delete(customers,ID);
+                            Console.WriteLine("");
+                            Selection();
+                            break;
+                        }
+                    case "l":
+                    case "L":
+                        {
+                            Console.Clear();
+                            List<Customer> customers = new List<Customer> { customer1, customer2, customer3, customer4 };
+                            CustomerManager customerManager = new CustomerManager();
+                            customerManager.Listing(customers);
+                            Console.WriteLine("");
+                            Selection();
+                            break;
+                        }
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+
+
+
+        }
+    }
+}
